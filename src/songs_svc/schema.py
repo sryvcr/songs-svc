dd7f814fc36fbaea8a35ea795aff56b0de179cb8
sryvcr <andresgx9@gmail.com>
2021-03-07 20:35:49 -0500

    Logic created to list-filter bands and insert new band

--- src/songs_svc/schema.py
+++ src/songs_svc/schema.py
@@ -1,13 +1,15 @@
 import graphene
-from songs_svc.songs_app.schemas.songs import Query as QuerySchema, CreateSong
+from songs_svc.songs_app.schemas.songs import Query as QuerySongs, CreateSong
+from songs_svc.songs_app.schemas.bands import Query as QueryBands, CreateBand
 
 
-class Query(QuerySchema, graphene.ObjectType):
+class Query(QuerySongs, QueryBands, graphene.ObjectType):
     pass
 
 
 class Mutation(graphene.ObjectType):
     insert_song = CreateSong.Field()
+    insert_band = CreateBand.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
