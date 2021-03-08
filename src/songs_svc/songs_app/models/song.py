64d175b9a2d3b1d27b6a1403aada52aa26fe66c6
sryvcr <andresgx9@gmail.com>
2021-03-07 20:09:22 -0500

    create band model and relation it with songs

--- src/songs_svc/songs_app/models/song.py
+++ src/songs_svc/songs_app/models/song.py
@@ -1,5 +1,5 @@
-from django.db import models
 import uuid
+from django.db import models
 
 
 class Song(models.Model):
@@ -8,14 +8,10 @@ class Song(models.Model):
     external_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     album = models.CharField(max_length=200)
-    band = models.CharField(max_length=200)
+    band = models.ForeignKey('songs_app.Band', related_name='song', on_delete=models.PROTECT)
     artist = models.CharField(max_length=200)
     length = models.CharField(max_length=10)
     genre = models.CharField(max_length=100)
     subgenre = models.CharField(max_length=100, blank=True)
-    similar_bands = models.TextField(blank=True)
     tags = models.TextField(blank=True)
     instruments = models.TextField(blank=True)
-
-    def __str__(self):
-        return self.name
--- src/songs_svc/songs_app/schemas/songs.py
+++ src/songs_svc/songs_app/schemas/songs.py
@@ -2,7 +2,8 @@ import graphene
 from graphene import relay, ObjectType
 from graphene_django.types import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
-from songs_svc.songs_app.models.songs import Song as SongModel
+from songs_svc.songs_app.models.song import Song as SongModel
+from songs_svc.songs_app.models.band import Band as BandModel
 
 
 class Song(DjangoObjectType):
@@ -11,10 +12,10 @@ class Song(DjangoObjectType):
         filter_fields = {
             'name': ['exact', 'icontains', 'istartswith'],
             'album': ['exact', 'icontains', 'istartswith'],
-            'band': ['exact', 'icontains', 'istartswith'],
             'genre': ['exact', 'icontains', 'istartswith'],
             'subgenre': ['exact', 'icontains', 'istartswith'],
-            'similar_bands': ['exact', 'icontains', 'istartswith'],
+            'band__name': ['exact', 'icontains', 'istartswith'],
+            'band__similar_bands': ['exact', 'icontains', 'istartswith'],
         }
         interfaces = (relay.Node, )
 
@@ -35,7 +36,6 @@ class CreateSong(relay.ClientIDMutation):
         length = graphene.String(required=True)
         genre = graphene.String(required=True)
         subgenre = graphene.String()
-        similar_bands = graphene.String()
         tags = graphene.String()
         instruments = graphene.String()
 
@@ -48,13 +48,14 @@ class CreateSong(relay.ClientIDMutation):
                                album, band,
                                artist, length,
                                genre, subgenre,
-                               similar_bands, tags,
-                               instruments, client_mutation_id=None):
+                               tags, instruments,
+                               client_mutation_id=None):
+        band = BandModel.objects.filter(name=band).first()
         new_song = SongModel(datetime=datetime, external_id=external_id,
                         name=name, album=album,
                         band=band, artist=artist,
                         length=length, genre=genre,
-                        subgenre=subgenre, similar_bands=similar_bands,
-                        tags=tags, instruments=instruments)
+                        subgenre=subgenre, tags=tags,
+                        instruments=instruments)
         new_song.save()
         return CreateSong(song=new_song)
